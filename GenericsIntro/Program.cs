05f1c3d2d3536bb30c1df6f6e3f20113ab9bbbd0
Ümit ARLI <wtfumit@outlook.com.tr>
2022-10-15 00:11:30 +0300

    15.10

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericsIntro;
+using System;
+
+
+namespace GenercsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Ümit");
+        }
+    }
+}
