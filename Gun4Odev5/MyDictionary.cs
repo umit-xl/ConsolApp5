6fda006199d9c7a99e82fa1ffb03044280756a9e
Ümit ARLI <wtfumit@outlook.com.tr>
2022-10-23 23:55:02 +0300

    15.10

---
+++ Gun4Odev5/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gun4Odev5
+{
+    internal class MyDictionary<Dkey , Dvalue>
+    {
+        KeyValuePair<Dkey , Dvalue>[] kullanicilar;
+
+        public MyDictionary()
+        {
+            kullanicilar = new KeyValuePair<Dkey , Dvalue>[0];
+        }
+
+      
+
+        public void AddManager()
+        {
+            KeyValuePair<Dkey, Dvalue>[] tempArray = kullanicilar;
+            kullanicilar = new KeyValuePair<Dkey, Dvalue>[kullanicilar.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                kullanicilar[i] = tempArray[i];
+            }
+
+        }
+
+        public void Add(Dkey _key, Dvalue _value)
+        {
+            AddManager();
+
+            kullanicilar[kullanicilar.Length - 1] = new KeyValuePair<Dkey, Dvalue>(_key, _value);
+
+        }
+
+        public void Listele()
+        {
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+    }
+}
---
+++ Gun4Odev5/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Gun4Odev5;
+using System.Collections.Generic;
+
+//Dictionary<int, string> yapilanIsSayisi = new Dictionary<int, string>();
+
+//yapilanIsSayisi.Add(10, "Ümit");
+//yapilanIsSayisi.Add(8, "Ceylin");
+
+//foreach (var sirala in yapilanIsSayisi)
+//{
+//    Console.WriteLine(sirala);
+//}
+
+//bunu kendin olusturacaksın
+
+MyDictionary<string, int> islemDurumu = new MyDictionary<string, int>();
+islemDurumu.Add("Ümit", 15);
+islemDurumu.Add("Ceylin", 10);
+islemDurumu.Add("Kiara", 5);
+
+islemDurumu.Listele();
+
+
