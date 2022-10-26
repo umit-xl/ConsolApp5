4dcc48bb591586afb310f20903254a6e75795b14
Ümit ARLI <wtfumit@outlook.com.tr>
2022-10-26 18:48:03 +0300

    26.10

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class GercekMusteri:Musteri
+    {
+     
+        public string Adi { get; set; }
+        public string soyadi { get; set; }
+        public string Tcno { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP2;
+
+GercekMusteri musteri1 = new GercekMusteri();
+musteri1.Id = 1;
+musteri1.MusteriNo = "12345";
+musteri1.Adi = "Ümit";
+musteri1.soyadi = "Arlı";
+musteri1.Tcno = "12344534";
+
+TuzelMusteri musteri2 = new TuzelMusteri();
+musteri2.Id = 2;
+musteri2.MusteriNo = "1234";
+musteri2.SirketAdi = "Samsung";
+musteri2.VergiNo = "345456";
+
+Musteri musteri3 = new GercekMusteri();
+Musteri musteri4 = new TuzelMusteri();
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(musteri4);
